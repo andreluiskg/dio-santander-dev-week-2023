0e3607dec494c4504ec29b9922f836a20eb06260
Andre Klem <andre@klem>
2025-04-30 16:48:10 -0300

    Segundo commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package me.dio.santander_dev_week_2023;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeek2023Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.santander_dev_week_2023;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantanderDevWeek2023ApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
